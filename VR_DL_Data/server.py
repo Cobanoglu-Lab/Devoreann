2d64e3f5edfff8a0fd31e16b98a175ed0c786097
Kevin VanHorn <kevin.vanhorn@utsouthwestern.edu>
2019-06-13 17:19:25 -0500

    Updated - Copyright.

--- VR_DL_Data/server.py
+++ VR_DL_Data/server.py
@@ -2,6 +2,12 @@
 Copyright © 2019, University of Texas Southwestern Medical Center. All rights reserved.
 Contributors: Kevin VanHorn, Meyer Zinn, Murat Can Cobanoglu
 Department: Lyda Hill Department of Bioinformatics
+
+Copyright © 2019, University of Texas Southwestern Medical Center. All rights reserved.
+Contributors: Kevin VanHorn, Meyer Zinn, Murat Can Cobanoglu
+Department: Lyda Hill Department of Bioinformatics.
+This software and any related documentation constitutes published and/or unpublished works and may contain valuable trade secrets and proprietary information belonging to The University of Texas Southwestern Medical Center (UT SOUTHWESTERN).  None of the foregoing material may be copied, duplicated or disclosed without the express written permission of UT SOUTHWESTERN.  IN NO EVENT SHALL UT SOUTHWESTERN BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF UT SOUTHWESTERN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  UT SOUTHWESTERN SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". UT SOUTHWESTERN HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+This software contains copyrighted materials from Oculus, Unity Technologies, Keras, TensorFlow, gRPC, NumPy, Matplotlib, OpenCV, Pyprind, Nvidia CUDA, wiki.unity3d.com, PyCharm, Visual Studio Community, and Google. Corresponding terms and conditions apply.
 """
 
 import grpc
